3bb2fae58686a37731ffd03b2d01149d90960ea7
Nataliya <Natalia.konyaeva@yandex.ru>
2022-09-04 12:01:26 +0300

    initial commit1

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿System.Console.WriteLine ("Введите любо целое положительное число");
+
+string number = Console.ReadLine();
+
+int number1 = int.Parse (number);
+
+int n = 1;
+
+bool not = true;
+
+Console.WriteLine("Чётные числа от 1 до " + number1);
+            while (n <= number1)
+            {
+                if (n % 2 != 1)
+                {
+                    Console.Write(n + ", ");
+                    bool = false;
+                }
+                n++;
+            }
+
+            if (not)
+            {
+                Console.WriteLine("Нет чётных чисел!");
+            }
+        
+        
+
+        
